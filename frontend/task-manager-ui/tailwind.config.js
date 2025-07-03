5cd405281f08d1b8cb7eecba22db11a9c123faba
DESKTOP-UQ5TGGD\LENOVO <hafiz300@outlook.com>
2025-07-03 19:27:48 +0100

    removed node modules from git

--- frontend/task-manager-ui/tailwind.config.js
+++ frontend/task-manager-ui/tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    "./index.html", // For the public/index.html if you use it
-    "./src/**/*.{js,jsx,ts,tsx}", // **This is critical for your React components**
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
